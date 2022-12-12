fb110056848d61e5829737c0fbd568da09f30532
Bernhard Frauendienst <git@nospam.obeliks.de>
2022-12-12 23:03:18 +0100

    Day 12 (skipped a few)

---
+++ day12/puzzle1.py
@@ -0,0 +1,77 @@
+import sys
+input = open('input' if len(sys.argv) == 1 else sys.argv[1]).read()
+
+class Point(object):
+    def __init__(self, x, y, h):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.visited = False
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return 31 * hash(self.x) + hash(self.y)
+
+    def can_step_up(self, other):
+        return other.h <= self.h + 1
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+S = None
+E = None
+
+offset = ord('a')
+def point(h, x, y):
+    global S, E
+    if h == 'S':
+        S = Point(x, y, ord('a')-offset)
+        return S
+    if h == 'E':
+        E = Point(x, y, ord('z')-offset)
+        return E
+    return Point(x, y, ord(h)-offset)
+
+grid = [[point(c, x, y) for x, c in enumerate(line)] for y, line in enumerate(input.splitlines())]
+assert S is not None
+assert E is not None
+
+def grid_width():
+    return len(grid[0])
+
+def grid_height():
+    return len(grid)
+
+def lower_neighbors(p):
+    if p.x > 0:
+        l = grid[p.y][p.x-1]
+        if l.can_step_up(p): yield l
+    if p.y > 0:
+        u = grid[p.y-1][p.x]
+        if u.can_step_up(p): yield u
+    if p.x < grid_width()-1:
+        r = grid[p.y][p.x+1]
+        if r.can_step_up(p): yield r
+    if p.y < grid_height()-1:
+        d = grid[p.y+1][p.x]
+        if d.can_step_up(p): yield d
+
+def find_shortest_path():
+    paths = [[E]]
+    while True:
+        for path in paths:
+            for n in lower_neighbors(path[-1]):
+                if n == S:
+                    path.reverse()
+                    return path
+                elif not n.visited:
+                    n.visited = True
+                    paths.append([*path, n])
+
+path = find_shortest_path()
+
+solution = len(path)
+
+print(solution)
